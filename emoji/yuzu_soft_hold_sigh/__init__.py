be679cbc86045a75de67d67b0cf6b10d34c666d6
Anyliew <tikin.liew@outlook.com>
2025-05-20 21:21:02 +0800

    新增柚子厨举牌

---
+++ emoji/yuzu_soft_hold_sigh/__init__.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+from meme_generator.tags import MemeTags
+
+
+img_dir = Path(__file__).parent / "images"
+
+
+def yuzu_soft_hold_sigh(images, texts: list[str], args):
+    text = texts[0]
+    frame = BuildImage.open(img_dir / "0.jpg")
+    try:
+        frame.draw_text(
+            (1150, 896, 3024, 1240),
+            text,
+            fill=(0, 0, 0),
+            allow_wrap=True,
+            max_fontsize=300,
+            min_fontsize=30,
+            lines_align="left",
+            font_families=["PangMenZhengDao-Cu"],
+        )
+    except ValueError:
+        raise TextOverLength(text)
+    return frame.save_jpg()
+
+
+add_meme(
+    "yuzu_soft_hold_sigh",
+    yuzu_soft_hold_sigh,
+    min_texts=1,
+    max_texts=1,
+    default_texts=["不要再涩涩了"],
+    keywords=["柚子厨举牌"],
+    date_created=datetime(2024, 12, 21),
+    #By Anyliew 2024年12月21日 22:10:12
+    date_modified=datetime(2024, 12, 21),
+)

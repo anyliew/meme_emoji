df93d88d518e0190d28e1b1256d31df096f591e6
Anyliew <tikin.liew@outlook.com>
2025-05-21 18:53:05 +0800

    重新制作 "鹿","🦌","鹿乃子乃子","鹿乃子"

--- emoji/deer/__init__.py
+++ emoji/deer/__init__.py
@@ -10,45 +10,26 @@ from meme_generator.utils import make_jpg_or_gif
 img_dir = Path(__file__).parent / "images"
 
 
-def deer_se(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def deer(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
     frame = BuildImage.open(img_dir / "0.png")
 
-    ta = "她{name}"
-    name = ta
-    if texts:
-        name = texts[0]
-    elif args.user_infos:
-        info = args.user_infos[0]
-        ta = "他" if info.gender == "male" else "她"
-        name = info.name or ta
-
-    text = f""
-    try:
-        frame.draw_text(
-            (50, 20, 700, 130),
-            text,
-            max_fontsize=70,
-            min_fontsize=20,
-            valign="bottom",
-        )
-    except ValueError:
-        raise TextOverLength(name)
-
     def make(imgs: list[BuildImage]) -> BuildImage:
-        img = imgs[0].convert("RGBA").circle().resize((275, 275))
-        return frame.copy().paste(img, (360, 40), alpha=True)
+        #头像尺寸
+        img = imgs[0].convert("RGBA").circle().resize((290, 290))
+        #头像坐标
+        return frame.copy().paste(img, (360, 3), alpha=True,below=True)
 
     return make_jpg_or_gif(images, make)
 
 
 add_meme(
-    "deer_se",
-    deer_se,
+    "deer",
+    deer,
     min_images=1,
     max_images=1,
     min_texts=0,
     max_texts=1,
-    keywords=["🦌","鹿"],
+    keywords=["鹿","🦌","鹿乃子乃子","鹿乃子"],
     date_created=datetime(2024, 7, 26),
     date_modified=datetime(2024, 7, 26),
 )

5dd8f4e980e2a7e11a0dfb363bbcd1e83d4d8a1e
Anyliew <tikin.liew@outlook.com>
2025-05-26 16:20:36 +0800

    新增表情挣扎 优化KFC头像

--- emoji/anyliew_struggling/__init__.py
+++ emoji/anyliew_struggling/__init__.py
@@ -5,32 +5,32 @@ from pil_utils import BuildImage
 
 from meme_generator import MemeArgsModel, add_meme
 from meme_generator.exception import TextOverLength
-from meme_generator.utils import make_png_or_gif
+from meme_generator.utils import make_jpg_or_gif
 
 img_dir = Path(__file__).parent / "images"
 
 
-def kfc_head(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def anyliew_struggling(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
     frame = BuildImage.open(img_dir / "0.png")
 
     def make(imgs: list[BuildImage]) -> BuildImage:
         #头像尺寸
-        img = imgs[0].convert("RGBA").circle().resize((500, 500))
-        img = img.rotate(15, expand=True)
+        img = imgs[0].convert("RGBA").circle().resize((300, 300))
+        img = img.rotate(25, expand=True)
         #头像坐标
-        return frame.copy().paste(img, (65, 65), alpha=True)
+        return frame.copy().paste(img, (275, 28), alpha=True,below=True)
 
-    return make_png_or_gif(images, make)
+    return make_jpg_or_gif(images, make)
 
 
 add_meme(
-    "kfc_head",
-    kfc_head,
+    "anyliew_struggling",
+    anyliew_struggling,
     min_images=1,
     max_images=1,
     min_texts=0,
     max_texts=1,
-    keywords=["kfc头像","KFC头像","肯德基头像"],
-    date_created=datetime(2024, 7, 26),
+    keywords=["挣扎"],
+    date_created=datetime(2025, 5, 26),
     date_modified=datetime(2025, 5, 26),
 )

d8f6d51b223aa636cc8d0df4b105356350a0d37f
Anyliew <tikin.liew@outlook.com>
2025-06-13 18:38:01 +0800

    添加新表情 弗洛洛说 菲比举牌

--- emoji/kurogames_phoebe_holdsign/__init__.py
+++ emoji/kurogames_phoebe_holdsign/__init__.py
@@ -10,19 +10,19 @@ from meme_generator.tags import MemeTags
 img_dir = Path(__file__).parent / "images"
 
 
-def kurogames_carlotta_holdsign(images, texts: list[str], args):
+def kurogames_phoebe_holdsign(images, texts: list[str], args):
     text = texts[0]
-    frame = BuildImage.open(img_dir / "0.png")
+    frame = BuildImage.open(img_dir / "0.jpg")
     try:
         frame.draw_text(
-            (196, 552, 785, 920),
+            (143, 512, 721, 855),
             text,
-            fill=(0, 0, 0),
+            fill=(72, 44, 41),
             allow_wrap=True,
             max_fontsize=120,
             min_fontsize=30,
-            lines_align="center",
-            font_families=["FZSJ-QINGCRJ"],
+            lines_align="left",
+            font_families=["FZShaoEr-M11S"],
         )
     except ValueError:
         raise TextOverLength(text)
@@ -30,13 +30,13 @@ def kurogames_carlotta_holdsign(images, texts: list[str], args):
 
 
 add_meme(
-    "kurogames_carlotta_holdsign",
-    kurogames_carlotta_holdsign,
+    "kurogames_phoebe_holdsign",
+    kurogames_phoebe_holdsign,
     min_texts=1,
     max_texts=1,
-    default_texts=["十连三金"],
-    keywords=["珂莱塔举牌"],
+    default_texts=["我是隐海修会的教士，菲比。岁主在上，愿你的旅途永远有爱与光明垂耀。"],
+    keywords=["菲比举牌"],
     tags=MemeTags.wuthering_waves,
-    date_created=datetime(2025, 5, 17),
-    date_modified=datetime(2025, 5, 17),
+    date_created=datetime(2025, 6, 13),
+    date_modified=datetime(2025, 6, 13),
 )
--- emoji/kurogames_phrolova_say/__init__.py
+++ emoji/kurogames_phrolova_say/__init__.py
@@ -10,19 +10,19 @@ from meme_generator.tags import MemeTags
 img_dir = Path(__file__).parent / "images"
 
 
-def kurogames_carlotta_holdsign(images, texts: list[str], args):
+def kurogames_phrolova_say(images, texts: list[str], args):
     text = texts[0]
-    frame = BuildImage.open(img_dir / "0.png")
+    frame = BuildImage.open(img_dir / "0.jpg")
     try:
         frame.draw_text(
-            (196, 552, 785, 920),
+            (1, 1, 990, 192),
             text,
             fill=(0, 0, 0),
             allow_wrap=True,
             max_fontsize=120,
-            min_fontsize=30,
+            min_fontsize=50,
             lines_align="center",
-            font_families=["FZSJ-QINGCRJ"],
+            font_families=["FZShaoEr-M11S"],
         )
     except ValueError:
         raise TextOverLength(text)
@@ -30,13 +30,13 @@ def kurogames_carlotta_holdsign(images, texts: list[str], args):
 
 
 add_meme(
-    "kurogames_carlotta_holdsign",
-    kurogames_carlotta_holdsign,
+    "kurogames_phrolova_say",
+    kurogames_phrolova_say,
     min_texts=1,
     max_texts=1,
-    default_texts=["十连三金"],
-    keywords=["珂莱塔举牌"],
+    default_texts=["怪鸟在鸣啸，时间到了。"],
+    keywords=["弗洛洛说"],
     tags=MemeTags.wuthering_waves,
-    date_created=datetime(2025, 5, 17),
-    date_modified=datetime(2025, 5, 17),
+    date_created=datetime(2025, 6, 13),
+    date_modified=datetime(2025, 6, 13),
 )

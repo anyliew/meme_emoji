dd253757a53246ee3ebfcecd36be80c1d13c0885
Anyliew <tikin.liew@outlook.com>
2025-06-16 22:35:37 +0800

    kurogames_lingyang_holdsign Photoshop重做素材 添加可爱的雪豹 优化代码

--- emoji/kurogames_lingyang_holdsign/__init__.py
+++ emoji/kurogames_lingyang_holdsign/__init__.py
@@ -12,21 +12,21 @@ img_dir = Path(__file__).parent / "images"
 
 def kurogames_lingyang_holdsign(images, texts: list[str], args):
     text = texts[0]
-    frame = BuildImage.open(img_dir / "0.png")
+    frame = BuildImage.open(img_dir / "0.jpg")
     try:
         frame.draw_text(
-            (13, 503, 563, 805),
+            (146, 465, 627, 890),
             text,
             fill=(0, 0, 0),
             allow_wrap=True,
             max_fontsize=120,
             min_fontsize=30,
-            lines_align="center",
-            font_families=["FZShaoEr-M11S"],
+            lines_align="left",
+            font_families=["FZKaTong-M19S"],
         )
     except ValueError:
         raise TextOverLength(text)
-    return frame.save_png()
+    return frame.save_jpg()
 
 
 add_meme(
@@ -34,9 +34,9 @@ add_meme(
     kurogames_lingyang_holdsign,
     min_texts=1,
     max_texts=1,
-    default_texts=["你必定歪我"],
+    default_texts=["我叫凌阳，比起我摘下狮头后的这幅模样，可能大家更习惯的，还是那位梅花桩上的“狮首”吧？希望相处之后，你能记住这个原原本本的我呀。"],
     keywords=["凌阳举牌", "雪豹举牌"],
     tags=MemeTags.wuthering_waves,
     date_created=datetime(2025, 5, 25),
-    date_modified=datetime(2025, 5, 25),
+    date_modified=datetime(2025, 6, 16),
 )

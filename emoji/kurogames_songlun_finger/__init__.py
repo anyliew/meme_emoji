2f714b418f3e526db5c13c1870eb910d472ffce3
Anyliew <tikin.liew@outlook.com>
2025-06-20 01:17:13 +0800

    kurogames_songlun 重做表情和素材图片； kurogames_songlun 改为 kurogames_songlun_finger ·

--- emoji/kurogames_songlun_finger/__init__.py
+++ emoji/kurogames_songlun_finger/__init__.py
@@ -11,8 +11,8 @@ from meme_generator.tags import MemeTags
 img_dir = Path(__file__).parent / "images"
 
 
-def kurogames_songlun(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
-    frame = BuildImage.open(img_dir / "0.png")
+def kurogames_songlun_finger(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+    frame = BuildImage.open(img_dir / "0.jpg")
 
     ta = "她"
     name = ta
@@ -23,34 +23,35 @@ def kurogames_songlun(images: list[BuildImage], texts: list[str], args: MemeArgs
         ta = "他" if info.gender == "male" else "她"
         name = info.name or ta
 
-    text = f""
+    text = f"原来{name}是鸣潮玩家\n是潮批啊~\n原来你也喜欢玩鸣潮"
     try:
         frame.draw_text(
-            (50, 20, 700, 130),
+            (3, 350, 482, 606),
             text,
-            max_fontsize=70,
-            min_fontsize=20,
-            valign="bottom",
+            max_fontsize=120,
+            min_fontsize=35,
+            lines_align="center",
+            font_families=["FZShaoEr-M11S"],
         )
     except ValueError:
         raise TextOverLength(name)
 
     def make(imgs: list[BuildImage]) -> BuildImage:
-        img = imgs[0].convert("RGBA").circle().resize((180, 180))
-        return frame.copy().paste(img, (500, 100), alpha=True)
+        img = imgs[0].convert("RGBA").circle().resize((300, 300))
+        return frame.copy().paste(img, (100, 40), alpha=True)
 
     return make_jpg_or_gif(images, make)
 
 
 add_meme(
-    "kurogames_songlun",
-    kurogames_songlun,
+    "kurogames_songlun_finger",
+    kurogames_songlun_finger,
     min_images=1,
     max_images=1,
-    min_texts=0,
+    min_texts=1,
     max_texts=1,
-    keywords=["松伦哥指", "潮批"],
+    keywords=["松伦指","松伦哥指", "李松伦指","潮批"],
     tags=MemeTags.wuthering_waves,
-    date_created=datetime(2025, 3, 7),
-    date_modified=datetime(2025, 3, 7),
+    date_created=datetime(2025, 6, 20),
+    date_modified=datetime(2025, 6, 20),
 )

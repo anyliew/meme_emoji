8723a9ee5d0894c0d97df4f3f78c24aa7282d78f
Anyliew <tikin.liew@outlook.com>
2025-07-01 09:52:15 +0800

    重做

--- emoji/kurogames_songlun_finger/__init__.py
+++ emoji/kurogames_songlun_finger/__init__.py
@@ -12,9 +12,9 @@ img_dir = Path(__file__).parent / "images"
 
 
 def kurogames_songlun_finger(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
-    frame = BuildImage.open(img_dir / "0.jpg")
+    frame = BuildImage.open(img_dir / "0.png")
 
-    ta = "她"
+    ta = "他"
     name = ta
     if texts:
         name = texts[0]
@@ -23,14 +23,14 @@ def kurogames_songlun_finger(images: list[BuildImage], texts: list[str], args: M
         ta = "他" if info.gender == "male" else "她"
         name = info.name or ta
 
-    text = f"原来{name}是潮批啊~"
+    text = f"难道说 \n {name}你是潮批?"
     try:
         frame.draw_text(
             (3, 350, 482, 606),
             text,
             fill=(0, 0, 0),
-            max_fontsize=120,
-            min_fontsize=35,
+            max_fontsize=100,
+            min_fontsize=20,
             lines_align="center",
             font_families=["FZShaoEr-M11S"],
         )
@@ -38,8 +38,8 @@ def kurogames_songlun_finger(images: list[BuildImage], texts: list[str], args: M
         raise TextOverLength(name)
 
     def make(imgs: list[BuildImage]) -> BuildImage:
-        img = imgs[0].convert("RGBA").circle().resize((300, 300))
-        return frame.copy().paste(img, (100, 40), alpha=True)
+        img = imgs[0].convert("RGBA").circle().resize((300, 300)))
+        return frame.copy().paste(img, (100, 40)), alpha=True, below=True)
 
     return make_jpg_or_gif(images, make)
 
@@ -49,10 +49,10 @@ add_meme(
     kurogames_songlun_finger,
     min_images=1,
     max_images=1,
-    min_texts=1,
+    min_texts=0,
     max_texts=1,
-    keywords=["松伦指","松伦哥指", "李松伦指","潮批"],
-    tags=MemeTags.wuthering_waves,
-    date_created=datetime(2025, 6, 20),
+    keywords=["松伦指","潮批"],
+    tags=MemeTags.genshin,
+    date_created=datetime(2025, 7, 1),
     date_modified=datetime(2025, 7, 1),
 )

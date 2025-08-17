9d7ea689d026ca888e902facde6d0fc3c6be43aa
Anyliew <tikin.liew@outlook.com>
2025-08-17 22:04:21 +0800

    add rabbit dog_face changed to dog_girl add dog_ear_hat

--- emoji/dog_girl/__init__.py
+++ emoji/dog_girl/__init__.py
@@ -8,7 +8,7 @@ from meme_generator import add_meme
 img_dir = Path(__file__).parent / "images"
 
 
-def dog_face(images: list[BuildImage], texts, args):
+def dog_girl(images: list[BuildImage], texts, args):
     img = images[0].convert("RGBA").square().resize((180, 180))
     frame = BuildImage.open(img_dir / "0.png")
     frame.paste(img, (312, 115), below=True)
@@ -16,11 +16,11 @@ def dog_face(images: list[BuildImage], texts, args):
 
 
 add_meme(
-    "dog_face",
-    dog_face,
+    "dog_girl",
+    dog_girl,
     min_images=1,
     max_images=1,
-    keywords=["🐶","狗"],
-    date_created=datetime(2025, 5, 25),
-    date_modified=datetime(2025, 5, 25),
+    keywords=["狗妹"],
+    date_created=datetime(2025, 8, 17),
+    date_modified=datetime(2025, 8, 17),
 )

6b747918da06ed85ca341782292998d1d8980181
Anyliew <tikin.liew@outlook.com>
2025-09-04 22:44:40 +0800

    重做 ikun_need_tv 坤坤想要

--- emoji/ikun_need_tv/__init__.py
+++ emoji/ikun_need_tv/__init__.py
@@ -13,11 +13,33 @@ img_dir = Path(__file__).parent / "images"
 def ikun_need_tv(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
     frame = BuildImage.open(img_dir / "0.png")
 
+    ta = "他"
+    name = ta
+    if texts:
+        name = texts[0]
+    elif args.user_infos:
+        info = args.user_infos[0]
+        ta = "他" if info.gender == "male" else "她"
+        name = info.name or ta
+
+    text = f"坤坤好喜欢\n想要一个{name}❤️~"
+    try:
+        frame.draw_text(
+            (1, 1, 572, 250),
+            text,
+            fill=(255, 255, 255),
+            allow_wrap=True,
+            max_fontsize=100,
+            min_fontsize=10,
+            lines_align="center",
+            font_families=["FZShaoEr-M11S"],
+        )
+    except ValueError:
+        raise TextOverLength(name)
+
     def make(imgs: list[BuildImage]) -> BuildImage:
-        #头像尺寸
-        img = imgs[0].convert("RGBA").resize((510, 510))
-        #头像坐标
-        return frame.copy().paste(img, (620, 120), alpha=True,below=True)
+        img = imgs[0].convert("RGBA").resize((518, 488))
+        return frame.copy().paste(img, (614, 118), alpha=True, below=True)
 
     return make_jpg_or_gif(images, make)
 
@@ -28,8 +50,8 @@ add_meme(
     min_images=1,
     max_images=1,
     min_texts=0,
-    max_texts=0,
-    keywords=["坤坤想要","蔡徐坤想要","爱坤想要"],
-    date_created=datetime(2025, 5, 30),
-    date_modified=datetime(2025, 5, 30),
+    max_texts=1,
+    keywords=["坤坤想要"],
+    date_created=datetime(2025, 9, 4),
+    date_modified=datetime(2025, 9, 4),
 )

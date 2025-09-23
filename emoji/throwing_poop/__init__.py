229957e6b9043b4e57f37b82cd3c11a23a3a6aed
Anyliew <tikin.liew@outlook.com>
2025-09-23 22:05:25 +0800

    PS D:\meme> date 2025年9月23日 22:04:32 ➕😆 添加新表情 cat_lick 猫舔 ➕😆 添加新表情 horse_riding 骑马 ➕😆 添加新表情 happy_national_day 国庆节快乐 ➕😆 表情 github 改为 throwing_poop 扔史 ➕😆 添加新表情 mi_leijun_holdsign 雷军举牌 ➕😆 表情 mi_monkey 优化素材猴子图标

--- emoji/throwing_poop/__init__.py
+++ emoji/throwing_poop/__init__.py
@@ -10,7 +10,7 @@ from meme_generator.utils import make_jpg_or_gif
 img_dir = Path(__file__).parent / "images"
 
 
-def github(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def throwing_poop(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
     frame = BuildImage.open(img_dir / "0.png")
 
     def make(imgs: list[BuildImage]) -> BuildImage:
@@ -23,7 +23,7 @@ def github(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
         result = result.paste(img2, (310, 553), alpha=True, below=True)
         
         # 第三个头像尺寸和坐标（更小，放在另一个位置）
-        img3 = imgs[0].convert("RGBA").resize((108, 108)).rotate(15, expand=True)
+        img3 = imgs[0].convert("RGBA").resize((108, 108)).rotate(20, expand=True)
         result = result.paste(img3, (297, 1003), alpha=True, below=True)
         
         return result
@@ -32,13 +32,13 @@ def github(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
 
 
 add_meme(
-    "github",
-    github,
+    "throwing_poop",
+    throwing_poop,
     min_images=1,
     max_images=1,
     min_texts=0,
     max_texts=0,
-    keywords=["Github", "github"],
+    keywords=["扔史"],
     date_created=datetime(2025, 9, 21),
-    date_modified=datetime(2025, 9, 21),
+    date_modified=datetime(2025, 9, 23),
 )

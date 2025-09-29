7f17ccb091afdbd0fc455d76b933f0afe98c8594
Anyliew <tikin.liew@outlook.com>
2025-09-29 23:21:40 +0800

    🐞🐛 national_day_plan 添加 --number 指定图片

--- emoji/national_day_plan/__init__.py
+++ emoji/national_day_plan/__init__.py
@@ -3,8 +3,15 @@ from pathlib import Path
 import random
 
 from pil_utils import BuildImage
+from pydantic import Field
 
-from meme_generator import MemeArgsModel, add_meme
+from meme_generator import (
+    MemeArgsModel,
+    MemeArgsType,
+    ParserArg,
+    ParserOption,
+    add_meme,
+)
 from meme_generator.exception import TextOverLength
 from meme_generator.utils import make_jpg_or_gif
 from meme_generator.tags import MemeTags
@@ -12,41 +19,71 @@ from meme_generator.tags import MemeTags
 img_dir = Path(__file__).parent / "images"
 
 
-def national_day_plan(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
-    # 随机选择 0-3 张图片
-    num_frames = random.randint(0, 3)
+help_text = "图片编号，范围为 0~3，0为随机"
+
+
+class Model(MemeArgsModel):
+    number: int = Field(0, description=help_text)
+
+
+args_type = MemeArgsType(
+    args_model=Model,
+    parser_options=[
+        ParserOption(
+            names=["-n", "--number"],
+            args=[ParserArg(name="number", value="int")],
+            help_text=help_text,
+        ),
+    ],
+)
+
+
+# 定义不同图片的参数配置
+frame_configs = [
+    {
+        "frame_file": "0.png",
+        "avatar_pos": (152, 28),
+        "avatar_size": (210, 210),
+        "text_bbox": (412, 28, 1175, 238),
+        "font_families": ["FZXS14"]
+    },
+    {
+        "frame_file": "1.png",  # 鸣潮
+        "avatar_pos": (18, 12),
+        "avatar_size": (125, 125),
+        "text_bbox": (152, 12, 694, 137),
+        "font_families": ["FZXS14"]
+    },
+    {
+        "frame_file": "2.png",  # 原神
+        "avatar_pos": (54, 25),
+        "avatar_size": (220, 220),
+        "text_bbox": (300, 25, 1000, 240),
+        "font_families": ["FZXS14"]
+    },
+    {
+        "frame_file": "3.png",  # 使命召唤
+        "avatar_pos": (38, 16),
+        "avatar_size": (118, 118),
+        "text_bbox": (168, 16, 697, 133),
+        "font_families": ["FZXS14"]
+    }
+]
+
+
+def national_day_plan(images: list[BuildImage], texts: list[str], args: Model):
+    total_num = len(frame_configs) - 1  # 0~3
     
-    # 定义不同图片的参数配置
-    frame_configs = [
-        {
-            "frame_file": "0.png",
-            "avatar_pos": (152, 28),
-            "avatar_size": (210, 210),
-            "text_bbox": (412, 28, 1175, 238),
-            "font_families": ["FZXS14"]
-        },
-        {
-            "frame_file": "1.png",  #鸣潮
-            "avatar_pos": (18, 12),
-            "avatar_size": (125, 125),
-            "text_bbox": (152, 12, 694, 137),
-            "font_families": ["FZXS14"]
-        },
-        {
-            "frame_file": "2.png", #原神
-            "avatar_pos": (54, 25),
-            "avatar_size": (220, 220),
-            "text_bbox": (300, 25, 1000, 240),
-            "font_families": ["FZXS14"]
-        },
-        {
-            "frame_file": "3.png", #使命召唤
-            "avatar_pos": (38, 16),
-            "avatar_size": (118, 118),
-            "text_bbox": (168, 16, 697, 133),
-            "font_families": ["FZXS14"]
-        }
-    ]
+    if args.number == 0:
+        # 随机选择配置
+        config_index = random.randint(0, total_num)
+    elif 1 <= args.number <= total_num + 1:
+        config_index = args.number - 1
+    else:
+        from meme_generator.exception import MemeFeedback
+        raise MemeFeedback(f"图片编号错误，请选择 0~{total_num + 1}")
+
+    config = frame_configs[config_index]
 
     ta = "他"
     name = ta
@@ -60,9 +97,6 @@ def national_day_plan(images: list[BuildImage], texts: list[str], args: MemeArgs
     text = f"{name}の国庆计划"
 
     def make(imgs: list[BuildImage]) -> BuildImage:
-        # 随机选择配置
-        config = random.choice(frame_configs)
-        
         # 加载底图
         frame = BuildImage.open(img_dir / config["frame_file"])
         
@@ -94,6 +128,7 @@ add_meme(
     max_images=1,
     min_texts=0,
     max_texts=1,
+    args_type=args_type,
     keywords=["国庆计划","国庆节计划"],
     date_created=datetime(2025, 9, 28),
     date_modified=datetime(2025, 9, 29),

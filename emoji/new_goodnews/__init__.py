d2375879fb03731ba59c79f9708f51f3c14b44d2
Anyliew <tikin.liew@outlook.com>
2025-10-03 23:38:56 +0800

    🐞🐛 xinxi_news改为new_goodnews 🐞🐛 new_goodnews优化，文字和name分开，完成去年狗哥的提议，添加两个文本内容自定义

--- emoji/new_goodnews/__init__.py
+++ emoji/new_goodnews/__init__.py
@@ -9,8 +9,7 @@ from meme_generator.utils import make_jpg_or_gif
 
 img_dir = Path(__file__).parent / "images"
 
-
-def xinxi_news(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def new_goodnews(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
     frame = BuildImage.open(img_dir / "0.png")
 
     ta = "她"
@@ -28,12 +27,30 @@ def xinxi_news(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
         frame.draw_text(
             (235, 615, 478, 659),
             text,
+            fill=(255, 223, 0),
             max_fontsize=100,
-            min_fontsize=20,
-            valign="bottom",
+            min_fontsize=10,
+            lines_align="center",
+            font_families=["FZXS14"], 
         )
     except ValueError:
         raise TextOverLength(name)
+
+    # 添加第二段文字
+    if len(texts) > 1:
+        try:
+            frame.draw_text(
+                (199, 708, 512, 923),  # 调整坐标区域以适应你的模板
+                texts[1],
+                fill=(255, 215, 0),
+                allow_wrap=True,
+                max_fontsize=60,
+                min_fontsize=10,
+                lines_align="center",
+                font_families=["FZXS14"], 
+            )
+        except ValueError:
+            raise TextOverLength(texts[1])
 
     def make(imgs: list[BuildImage]) -> BuildImage:
         #头像尺寸
@@ -45,13 +62,14 @@ def xinxi_news(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
 
 
 add_meme(
-    "xinxi_news",
-    xinxi_news,
+    "new_goodnews",
+    new_goodnews,
     min_images=1,
     max_images=1,
     min_texts=0,
-    max_texts=1,
+    max_texts=2,
     keywords=["新喜报"],
+    default_texts=["天命之人","喜报传佳讯\n福星高照\n满门庭"],
     date_created=datetime(2024, 7, 26),
-    date_modified=datetime(2024, 7, 26),
+    date_modified=datetime(2025, 10, 3),
 )

5a2392b0d10e905328db61c317b419c7a29af895
Anyliew <tikin.liew@outlook.com>
2026-02-24 21:28:13 +0800

    重做 爱弥斯捧

--- emoji/kurogames_aemeath_holding/__init__.py
+++ emoji/kurogames_aemeath_holding/__init__.py
@@ -1,25 +1,26 @@
 from datetime import datetime
 from pathlib import Path
 
+from PIL.Image import Image as IMG
 from pil_utils import BuildImage
 
-from meme_generator import MemeArgsModel, add_meme
-from meme_generator.exception import TextOverLength
-from meme_generator.utils import make_png_or_gif
-from meme_generator.tags import MemeTags
+from meme_generator import add_meme
+from meme_generator.utils import save_gif
 
 img_dir = Path(__file__).parent / "images"
 
 
-def kurogames_aemeath_holding(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def kurogames_aemeath_holding(images: list[BuildImage], texts, args):
+    img = images[0].convert("RGBA")
     frame = BuildImage.open(img_dir / "0.png")
-
-    def make(imgs: list[BuildImage]) -> BuildImage:
-        #头像尺寸
-        img = imgs[0].convert("RGBA").circle().resize((370, 370))
-        return frame.copy().paste(img, (911, 685), alpha=True,below=True)
-
-    return make_png_or_gif(images, make)
+    frames: list[IMG] = []
+    for i in range(0, 360, 10):
+        frames.append(
+            frame.copy()
+            .paste(img.rotate(-i).resize((91, 91)), (43, 159), below=True)
+            .image
+        )
+    return save_gif(frames, 0.05)
 
 
 add_meme(
@@ -27,10 +28,7 @@ add_meme(
     kurogames_aemeath_holding,
     min_images=1,
     max_images=1,
-    min_texts=0,
-    max_texts=1,
     keywords=["爱弥斯捧"],
-    tags=MemeTags.wuthering_waves,
-    date_created=datetime(2026, 2, 7),
-    date_modified=datetime(2026, 2, 7),
+    date_created=datetime(2026, 2, 24),
+    date_modified=datetime(2026, 2, 24),
 )

3c56264f5586d2ef3847b0aecdf2e581617c1a57
Anyliew <tikin.liew@outlook.com>
2026-04-17 15:02:12 +0800

    新增抓住、三十亿报错、迷迷帽、初音说、春日燕归来； 优化steal_two

--- emoji/steal_two/__init__.py
+++ emoji/steal_two/__init__.py
@@ -2,29 +2,20 @@ from datetime import datetime
 from pathlib import Path
 
 from pil_utils import BuildImage
-from meme_generator import MemeArgsModel, add_meme
-from meme_generator.exception import TextOverLength
-from meme_generator.utils import make_png_or_gif
+from meme_generator import add_meme
 
 img_dir = Path(__file__).parent / "images"
 
 
-def steal_two(images: list[BuildImage], texts: list[str], args: MemeArgsModel):
+def steal_two(images: list[BuildImage], texts: list[str], args):
     frame = BuildImage.open(img_dir / "0.png")
-
-    def make(imgs: list[BuildImage]) -> BuildImage:
-        # 处理三个头像
-        img0 = imgs[0].convert("RGBA").circle().resize((131, 131))
-        img1 = imgs[1].convert("RGBA").circle().resize((140, 140))
-        img2 = imgs[2].convert("RGBA").circle().resize((125, 125))
-
-        frame_copy = frame.copy()
-        frame_copy.paste(img0, (253, 78), alpha=True, below=True)
-        frame_copy.paste(img1, (11, 304), alpha=True, below=True)
-        frame_copy.paste(img2, (532, 387), alpha=True, below=True)
-        return frame_copy
-
-    return make_png_or_gif(images, make)
+    img0 = images[0].convert("RGBA").circle().resize((131, 131))
+    img1 = images[1].convert("RGBA").circle().resize((140, 140))
+    img2 = images[2].convert("RGBA").circle().resize((125, 125))
+    frame.paste(img0, (253, 78), alpha=True)
+    frame.paste(img1, (11, 304), alpha=True)
+    frame.paste(img2, (532, 387), alpha=True)
+    return frame.save_jpg()
 
 
 add_meme(
@@ -34,5 +25,5 @@ add_meme(
     max_images=3,
     keywords=["双偷"],
     date_created=datetime(2026, 4, 11),
-    date_modified=datetime(2026, 4, 11),
+    date_modified=datetime(2026, 4, 17),
 )
